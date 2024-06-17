2b9c7282980ce1bc30a8597bf6e0d0d3f1ef8854
parknamjun <njpark@hyosung.com>
2024-06-17 16:34:15 +0900

    Google direction/route 테스트, 한국에서는 안됨...북한도 되는데...

---
+++ src/test/java/kr/giljabi/api/controller/google/RouteRequest.java
@@ -0,0 +1,24 @@
+package kr.giljabi.api.controller.google;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @Author : njpark@hyosung.com
+ * @Date : 2024.06.17
+ * @Description
+ */
+public class RouteRequest {
+    @SerializedName("origin")
+    Waypoint origin;
+    @SerializedName("destination")
+    Waypoint destination;
+    @SerializedName("travelMode")
+    String travelMode;
+
+    public RouteRequest(Waypoint origin, Waypoint destination, String travelMode) {
+        this.origin = origin;
+        this.destination = destination;
+        this.travelMode = travelMode;
+    }
+}
+
