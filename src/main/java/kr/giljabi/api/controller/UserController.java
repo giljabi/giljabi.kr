46f9897742ae66488c6b99111e42f21bf6490486
parknamjun <njpark@hyosung.com>
2024-08-28 17:13:36 +0900

    헷갈리지 않게 타임리프 giljabi2 파일명 변경

--- src/main/java/kr/giljabi/api/controller/UserController.java
+++ src/main/java/kr/giljabi/api/controller/UserController.java
@@ -2,9 +2,7 @@ package kr.giljabi.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -36,7 +34,7 @@ public class UserController {
     @RequestMapping("/manager/giljabi2")
     public String goManagerGiljabi2(){
         log.info("giljabi2");
-        return "manager/giljabi2";
+        return "manager/giljabi2-admin";
     }
 
 }
